e5b9621c7e5a15574fa4053d5bef1d0737f804ad
carolvfs <40553909+carolvfs@users.noreply.github.com>
2018-10-03 13:03:29 -0300

    Update simulador_cache.py

--- simulador_cache.py
+++ simulador_cache.py
@@ -1,14 +1,83 @@
 from objetos import *
 
+# Chamar este script com redirecionamento da saida para um arquivo texto.
+# Ex.: phython simulador_cache.py > saida.txt
 
-ch = Cache(16, 'direto')
-#ch = Cache(16, 'associativo')
-#ch = Cache(16, 'associativo por conjunto')
-
+# Cria a memoria Principal e define qual o nome do arquivo texto a ser lido como entrada.
 mp = MemoriaPrincipal('./blocosMP.txt')
+
+# Cria o controlador da cache.
 c = Controlador()
 
-#resultado_da_busca = c.procura_dados(mp.matriz, ch.matriz, ch.metodo, 'fifo')
-#resultado_da_busca = c.procura_dados(mp.matriz, ch.matriz, ch.metodo, 'lru')
+# Os metodos serao chamados para cada politica de substituicao.
+# Nao e necessario realizar alteracoes.
+
+print('\n******************************************************************')
+print(' >> Executando o Simulador...')
+print(' >> Alunas Carolina Veiga e Gyslla Vasconcelos')
+print('******************************************************************')
+
+print('\n******************************************************************')
+print(' >> Método Direto')
+print('******************************************************************')
+
+ch = Cache(16, 'direto')
+resultado_da_busca = c.procura_dados(mp.matriz, ch.matriz, ch.metodo, 'none')
+
+
+print('\n******************************************************************')
+print(' >> Método Associativo/FIFO')
+print('******************************************************************')
+
+ch = Cache(16, 'associativo')
+resultado_da_busca = c.procura_dados(mp.matriz, ch.matriz, ch.metodo, 'fifo')
+
+print('\n******************************************************************')
+print(' >> Método Associativo/LRU')
+print('******************************************************************')
+
+
+ch = Cache(16, 'associativo')
+resultado_da_busca = c.procura_dados(mp.matriz, ch.matriz, ch.metodo, 'lru')
+
+print('\n******************************************************************')
+print(' >>  Método Associativo/LFU')
+print('******************************************************************')
+
+ch = Cache(16, 'associativo')
 resultado_da_busca = c.procura_dados(mp.matriz, ch.matriz, ch.metodo, 'lfu')
-#resultado_da_busca = c.procura_dados(mp.matriz, ch.matriz, ch.metodo, 'random')
+
+print('\n******************************************************************')
+print(' >> Método Associativo/RANDOM')
+print('******************************************************************')
+
+ch = Cache(16, 'associativo')
+resultado_da_busca = c.procura_dados(mp.matriz, ch.matriz, ch.metodo, 'random')
+
+print('\n******************************************************************')
+print(' >> Método Associativo por Conjunto/FIFO')
+print('******************************************************************')
+
+ch = Cache(16, 'associativo por conjunto')
+resultado_da_busca = c.procura_dados(mp.matriz, ch.matriz, ch.metodo, 'fifo')
+
+print('\n******************************************************************')
+print(' >> Método Associativo por Conjunto/LRU')
+print('******************************************************************')
+
+ch = Cache(16, 'associativo por conjunto')
+resultado_da_busca = c.procura_dados(mp.matriz, ch.matriz, ch.metodo, 'lru')
+
+print('\n******************************************************************')
+print(' >> Método Associativo por Conjunto/LFU')
+print('******************************************************************')
+
+ch = Cache(16, 'associativo por conjunto')
+resultado_da_busca = c.procura_dados(mp.matriz, ch.matriz, ch.metodo, 'lfu')
+
+print('\n******************************************************************')
+print(' >> Método Associativo por Conjunto/RANDOM')
+print('******************************************************************')
+
+ch = Cache(16, 'associativo por conjunto')
+resultado_da_busca = c.procura_dados(mp.matriz, ch.matriz, ch.metodo, 'random')
